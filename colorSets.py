229b834e1b8b4ead4b15b2e39476a87317c89e78
Peter Jenei <peter.jenei@pronovix.com>
2024-06-01 15:12:32 +0200

    Add license to project

--- colorSets.py
+++ colorSets.py
@@ -1,3 +1,8 @@
+# This file is part of the WeatherStation project
+# and is licensed under the MIT License. For more details,
+# see the LICENSE file in the root directory of this project or visit
+# https://github.com/jenei-peter-dorozsma/WeatherStation
+
 class Colors:
     green = [
         (0, 238, 0, 255),  # Light green: #00ee00
